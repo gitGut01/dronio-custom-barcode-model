8a9106496518bf9e07b4c51d79a4337ff65629fb
gitGut01 <sile@computas.com>
2025-12-25 07:37:21 +0100

    add vocab

--- transformer_model_deep/vocab.py
+++ transformer_model_deep/vocab.py
@@ -3,8 +3,17 @@ from __future__ import annotations
 from typing import Dict, List, Tuple
 
 
-def code128_alphabet() -> str:
+def code128_alphabet_old() -> str:
     return "".join(chr(i) for i in range(32, 127))
+
+
+def code128_alphabet() -> str:
+    tags = ["[EAN13]", "[EAN8]", "[UPCA]", "[CODE39]", "[CODE128]"]
+    chars = [chr(i) for i in range(32, 127)]
+
+    full_vocab = tags + chars
+    char_to_idx = {char: i for i, char in enumerate(full_vocab)}
+    return char_to_idx
 
 
 def build_vocab_from_alphabet(alphabet: str) -> Tuple[Dict[str, int], Dict[int, str]]:
